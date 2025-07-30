a598634785d2e9a88ba73468a5cde08fa2efe0a7
catpho <cathyp98@outlook.com>
2025-07-30 14:33:49 -0400

    cleaner header

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,32 @@
 	</p>
 	<Header />
 
+	<div
+		style="  background-color:#eeeeee; box-shadow: 0 4px 14px rgba(0, 0, 0, 0.25); margin-bottom: 100px;"
+	>
+		<div class="m-6 ml-8 flex items-start text-sm text-gray-800">
+			<!-- My Warehouse -->
+			<div>
+				<p class="text-xs text-gray-600">My Warehouse</p>
+				<div class="flex items-center gap-1 font-semibold">
+					<i class="fa-solid fa-location-dot text-gray-700"></i>
+					<span class="text-black">Grand Rapids</span>
+				</div>
+				<p class="mt-1 text-xs text-gray-600">Executive Member hours start at 9AM</p>
+				<p class="text-xs text-gray-600">Gold Star & Business hours start at 10AM</p>
+			</div>
+
+			<!-- Delivery Location -->
+			<div>
+				<p class="text-xs text-gray-600">Delivery Location</p>
+				<div class="flex items-center justify-end font-semibold">
+					<i class="fa-solid fa-box text-gray-700"></i>
+					<span class="text-black">48201</span>
+				</div>
+			</div>
+		</div>
+	</div>
+
 	<main>
 		<slot />
 	</main>
@@ -38,7 +64,7 @@
 		flex-direction: column;
 		padding: 1rem;
 		width: 100%;
-		max-width: 64rem;
+
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	import logo from '$lib/images/Costco-Logo-Registered.svg';
-	import github from '$lib/images/github.svg';
+
 	import { Search } from 'flowbite-svelte';
 </script>
 
@@ -9,54 +9,73 @@
 	<!--the container for this nav bar needs to be adjusted to be consistent in style regardless of the browser/screen size	-->
 	<!--make sure that number of list items change when the screen is adjusted	-->
 	<nav class="top-nav">
-		<ul>
-			<li>
-				<a href="/">Costco Next</a>
-			</li>
-			<li>
-				<a href="/">While Supplies Last</a>
-			</li>
-			<li>
-				<a href="/">Online-Only</a>
-			</li>
-			<li>
-				<a href="/">Treasure Hunt</a>
-			</li>
-			<li>
-				<a href="/">What's New</a>
-			</li>
-			<li>
-				<a href="/">Member Favorites</a>
-			</li>
-			<li>
-				<a href="/">Get Email Offers</a>
-			</li>
-			<li>
-				<a href="/">Customer Service</a>
-			</li>
-			<li>
-				<a href="/">US(dropdownlist)</a>
-			</li>
-		</ul>
+		<a href="/">Costco Next</a>
+		<a href="/">While Supplies Last</a>
+		<a href="/">Online-Only</a>
+		<a href="/">Treasure Hunt</a>
+		<a href="/">What's New</a>
+		<a href="/">Member Favorites</a>
+		<a href="/">Get Email Offers</a>
+		<a href="/">Customer Service</a>
+		<a href="/">US (dropdownlist)</a>
 	</nav>
 </header>
-<nav class="main-nav">
+<nav class="main-nav p-2 pl-6 pr-6">
 	<a class="costco-logo" href="https://www.costco.com/">
 		<img src={logo} alt="Costco Logo" />
 	</a>
 
-	<Search size="lg" clearable />
+	<div class="flex-grow">
+		<Search placeholder="Search Costco" class="w-full" />
+	</div>
 
-	<a href="/">Sign In/Register</a>
+	<a href="/">Account</a>
 
 	<a href="/">Orders & Returns</a>
 
-	<span>Cart(with logo)</span>
+	<a href="/">
+		<svg
+			class="h-[34px] w-[34px] text-gray-800"
+			aria-hidden="true"
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			fill="none"
+			viewBox="0 0 24 24"
+		>
+			<path
+				stroke="#1566aa"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="1.3"
+				d="M5 4h1.5L9 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm-8.5-3h9.25L19 7H7.312"
+			/>
+		</svg>
+		Cart</a
+	>
 </nav>
 <nav class="bottom-nav">
-	<ul>
+	<ul class="bottom-nav-list space-x-4">
 		<li>
-			<a href="/">Shop</a>
+			<a href="/"
+				><svg
+					class="h-6 w-6 text-gray-800 dark:text-white"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					fill="none"
+					viewBox="0 0 24 24"
+				>
+					<path
+						stroke="currentColor"
+						stroke-linecap="round"
+						stroke-width="2"
+						d="M5 7h14M5 12h14M5 17h14"
+					/>
+				</svg>
+				Shop</a
+			>
 		</li>
 		<li>
 			<a href="/">Grocery</a>
@@ -80,7 +99,16 @@
 			<a href="/">Services</a>
 		</li>
 		<li>
-			<a href="/">US(dropdownlist)</a>
+			<a href="/">Photo</a>
+		</li>
+		<li>
+			<a href="/">Travel</a>
+		</li>
+		<li>
+			<a href="/">Membership</a>
+		</li>
+		<li>
+			<a href="/">Locations</a>
 		</li>
 	</ul>
 </nav>
@@ -93,6 +121,7 @@
 	.costco-logo img {
 		width: 12rem;
 		height: 6rem;
+
 		object-fit: contain;
 	}
 
@@ -105,15 +134,16 @@
 
 	.top-nav {
 		display: flex;
-		justify-content: center;
+		justify-content: right;
 		align-items: center;
 		width: 100%;
 		background: #eeeeee;
-		padding: 0;
+		padding: 1rem;
 	}
 
 	.top-nav a {
 		color: #1566aa;
+		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
 	}
 
 	.bottom-nav {
@@ -127,6 +157,8 @@
 
 	.bottom-nav a {
 		color: white;
+		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+		font-size: large;
 	}
 
 	.main-nav {
@@ -137,14 +169,19 @@
 		background: #eeeeee;
 	}
 
-	ul {
+	.main-nav a {
+		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+		color: #1566aa;
+		font-size: large;
+	}
+
+	.bottom-nav-list {
 		position: relative;
-		padding: 0;
-		margin: 0;
 		height: 3em;
 		display: flex;
-		justify-content: center;
+		justify-content: left;
 		align-items: center;
+		margin-left: 25px;
 		list-style: none;
 		width: 100%;
 		background: var(--background);
@@ -156,28 +193,17 @@
 		height: 100%;
 	}
 
-	li[aria-current='page']::before {
-		--size: 6px;
-		content: '';
-		width: 0;
-		height: 0;
-		position: absolute;
-		top: 0;
-		left: calc(50% - var(--size));
-		border: var(--size) solid transparent;
-		border-top: var(--size) solid var(--color-theme-1);
-	}
-
 	nav a {
 		display: flex;
 		height: 100%;
 		align-items: center;
 		padding: 0 0.5rem;
 		color: var(--color-text);
-		font-weight: 700;
+		font-weight: normal;
 		font-size: 0.8rem;
-		text-transform: uppercase;
-		letter-spacing: 0.1em;
+		font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif !important;
+		text-transform: none;
+		letter-spacing: normal;
 		text-decoration: none;
 		transition: color 0.2s linear;
 	}
