35e77c0fc8caa8fa61b7de17bbb7102bfab70ed7
Cathy Phan <cathyp98@outlook.com>
2025-08-11 11:31:36 -0400

    added search icon to search footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,9 @@
 <script>
 	import { Search } from 'flowbite-svelte';
+	 let location = '';
+  function search() {
+    alert(`Searching for: ${location}`);
+  }
 </script>
 
 <footer>
@@ -12,24 +16,47 @@
 
 		<div>
 			<h2 class="text-[20px]">Find a Warehouse</h2>
+
+		<div class="mt-[15px] w-[500px] relative">
 			<input
 				type="text"
-				id="location"
-				name="location"
+				bind:value={location}
 				placeholder="City, State, or Zip"
-				class="mt-[15px] w-[600px] rounded border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+				class="w-full rounded border border-gray-300 shadow-sm pr-10 focus:outline-none focus:ring-2 focus:ring-blue-500"
 			/>
+			
+			<button
+				type="button"
+				on:click={search}
+				class="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-blue-500 focus:outline-none"
+			>
+				<svg class="w-6 h-6 text-gray-800 dark:text-blue" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+				<path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"/>
+				</svg>
+
+			</button>
+			</div>
 		</div>
 
 		<div>
 			<h3 class="text-[20px]">Get Email Offers</h3>
-			<input
-				type="text"
-				id="location"
-				name="location"
-				placeholder="Enter your email"
-				class="mt-[15px] w-[500px] rounded border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-			/>
+			<div class="mt-[15px] flex w-[500px]">
+				<input
+					type="text"
+					id="location"
+					name="location"
+					placeholder="Enter your email"
+					class="flex-1 rounded-l border border-gray-300 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+				/>
+				<button
+					class="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+				>
+					Search
+				</button>
+				</div>
+
+			
+		
 		</div>
 	</div>
 </footer>
